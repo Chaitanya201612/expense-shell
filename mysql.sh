f618b14f3a41457eab17962a3d8a737436b77867
chaitanya12206 <chaitanya12201@gmail.com>
2024-09-12 15:52:59 +0530

    shell

--- mysql.sh
+++ mysql.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-LOGS_FOLDER=/"var/log/expense"
+LOGS_FOLDER="/var/log/expense"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date +%Y-%m-%D-%H-%M-%S)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
 
@@ -15,37 +15,44 @@ Y="\e[33m"
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
-    echo -e "$R please run this script with root privileges $N" | tee -a $LOG_FILE
-    exit 1
+        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
+        exit 1
     fi
 }
 
 VALIDATE(){
-    if [$1 -ne 0]
+    if [ $1 -ne 0 ]
     then
-    echo -e "$2 is...$R FAILED $N" | tee -a $LOG_FILE
-    exit 1
+        echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
+        exit 1
     else
-    echo -e "$2 is..$G SUCCESS $N" | tee -a $LOG_FILE
+        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
-echo "Script started executing at: $(date)"
- 
+
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
 CHECK_ROOT
 
-dnf install mysql-server -y
-VALIDATE $? "Intalling mysql server"
+dnf install mysql-server -y &>>$LOG_FILE
+VALIDATE $? "Installing MySQL Server"
 
-systemctl enable mysqld
-VALIDATE $? "Enable MYSQL Server"
+systemctl enable mysqld &>>$LOG_FILE
+VALIDATE $? "Enabled MySQL Server"
 
-systemctl start mysqld
-VALIDATE $? "Started MYSQL Server"
+systemctl start mysqld &>>$LOG_FILE
+VALIDATE $? "Started MySQL server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1
-VALIDATE $? "Setting up root password"
+mysql -h mysql.daws81s.online -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    echo "MySQL root password is not setup, setting now" &>>$LOG_FILE
+    mysql_secure_installation --set-root-pass ExpenseApp@1
+    VALIDATE $? "Setting UP root password"
+else
+    echo -e "MySQL root password is already setup...$Y SKIPPING $N" | tee -a $LOG_FILE
+fi
 
-
-
-
-
+# Assignment
+# check MySQL Server is installed or not, enabled or not, started or not
+# implement the above things
